ef6bbc81d81257699d4a0bbfc44882500e351a83
varsita1104 <varsita1104@gmail.com>
2025-01-27 23:04:56 +0530

    Base Setup

---
+++ src/test/java/com/simpleblog/hello_blog/HelloBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleblog.hello_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
